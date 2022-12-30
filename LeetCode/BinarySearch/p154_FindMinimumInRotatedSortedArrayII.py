3ae650e1c98132491fc7a2c4848948518e794cd1
IronmanJay <1975686676@qq.com>
2022-12-31 01:24:22 +0800

    2022-12-31

---
+++ LeetCode/BinarySearch/p154_FindMinimumInRotatedSortedArrayII.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author：IronmanJay
+# email：1975686676@qq.com
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[right]:
+                right = mid
+            elif nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right-=1
+        return nums[left]
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums = [1,3,5]
+    res = solution.findMin(nums)
+    print(res)
